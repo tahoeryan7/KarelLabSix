3a1ca26d4f2a3c015db42d2cc927d37305aa0af1
tahoeryan7 <31353254+tahoeryan7@users.noreply.github.com>
2017-09-25 11:58:15 -0700

    done

--- BeeperBot.java
+++ BeeperBot.java
@@ -14,15 +14,38 @@ public class BeeperBot extends Robot
     public BeeperBot(int st, int av, Direction dir, int numBeepers) {
         super(st, av, dir, numBeepers);
     }
-    
+    int x=0;
     public void findAndCountBeepers() {
-        
+        for(int i=0; (i < 10); i++){
+            pickUpBeeper();
+        }
     }
-    
+    public void pickUpBeeper(){
+        while(!nextToABeeper()){
+            move();
+        }
+        if (nextToABeeper()){
+            turnLeft();
+        }
+        while(nextToABeeper()){
+            pickBeeper();
+            x++;
+            move();
+        }
+        turnLeft();
+        turnLeft();
+        while(frontIsClear()){
+            move();
+        }
+        turnLeft();
+    }
     public int[] getBeeperList() {
             //This will be used to print the beepers found.  No need to modify.
+            for(int i, index <arraygerBeeperList.length; index++)
+            
+            
             return beeperList;
     }
-   
+    
 }
 
